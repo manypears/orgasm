ea64a3606439fe3ec3aaa77737f706482c94525b
manypears <notartjosh@gmail.com>
2024-10-04 12:44:46 +0200

    remove gpt weird vite file

--- panorama/vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/orgasm/', // This should match your GitHub repository name
-  // You can add other configurations here if needed
-});
